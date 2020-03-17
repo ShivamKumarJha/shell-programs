802e0508297052c5a19773e577709b492d421cfb
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-17 13:00:40 +0530

    [ShivamKumarJha] Add temperaturConversion.sh. Script to convert temperature in between Celsius & Fahrenheit.
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ 3.temperaturConversion.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+while [[ 1 -gt 0 ]]; do
+    printf "\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\n"
+    read -p "Choice: " choice
+    if [[ $choice == 1 ]]; then
+        read -p "Enter value in Celsius: " value
+        conv=$(awk "BEGIN {print ( ($value*9/5)+32 )}")
+        echo "Fahrenheit: $conv"
+        break
+    elif [[ $choice == 2 ]]; then
+        read -p "Enter value in Fahrenheit: " value
+        conv=$(awk "BEGIN {print ( ($value-32)*5/9 )}")
+        echo "Celsius: $conv"
+        break
+    else
+        echo "Wrong choice!"
+    fi
+done
