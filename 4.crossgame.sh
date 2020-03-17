943606f3c69b2b76dde8d76b71d2486ffae115a2
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-17 16:24:14 +0530

    [ShivamKumarJha] Refactor crossgame.sh: already has data or not
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- 4.crossgame.sh
+++ 4.crossgame.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 declare -a sq
+index=0
 for ((i=1;i<=9;i++)); do
     sq[$i]=$i
 done
@@ -15,10 +16,21 @@ printboard() {
 
 while [[ 1 -gt 0 ]]; do
     printboard
+    # user i/p
     read -p "Enter choice: " ch
-    sq[$ch]="X"
+    index=$ch
+    if [[ ${sq[$index]} != "X" ]]; then
+        sq[$index]="X"
+    else
+        echo "Already has data. Repeating"
+    fi
+    # computer choice
     comp=$(shuf -i 1-9 -n 1)
-    echo "AI: $comp"
-    sq[$comp]="O"
+    index=$comp
+    if [[ ${sq[$index]} != "O" ]]; then
+        sq[$index]="O"
+    else
+        echo "Already has data. Repeating"
+    fi
     printboard
 done
