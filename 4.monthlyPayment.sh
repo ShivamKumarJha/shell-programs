afb3475c7515d692e86116fdc3aeee96ed6cb757
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-17 13:09:42 +0530

    [ShivamKumarJha] Add monthlyPayment.sh. A script to calculate interest.
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ 4.monthlyPayment.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -z "$3" ]; then
+    echo "Enter 3 args as <P> <Y> <R>"
+    exit 1
+fi
+
+p="$1"
+y="$2"
+r="$3"
+
+n=$((12*$y))
+r2=$(awk "BEGIN {print ( $r/(12*100) )}")
+payment=$(awk "BEGIN {print ( $p*$r2/(1-(1+$r2)**(-$n)) )}")
+echo $payment
