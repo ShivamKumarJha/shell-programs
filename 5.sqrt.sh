a6568041f2933f09ba7b87bcca62e035eaccba15
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-17 14:41:16 +0530

    [ShivamKumarJha] Add sqrt.sh. Script to find square root of a number.
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ 5.sqrt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    read -p "Enter the number: " num
+else
+    num="$1"
+fi
+if [[ $num -lt 0 ]]; then
+    echo "Invalid number"
+    exit 1
+fi
+
+sqrt=$(awk "BEGIN {print ( $num ** 0.5 )}")
+echo "Square root of $num is $sqrt"
