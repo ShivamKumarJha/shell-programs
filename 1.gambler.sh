e69946a7a927e71d26a9c61feab74315e9da5b15
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-17 11:19:37 +0530

    [ShivamKumarJha] Add script for gambler program
     * Usage:
       ./1.gambler.sh or 1.gambler.sh <stake> <goal>
    
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ 1.gambler.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    read -p "Enter your stake:" stake
+else
+    stake="$1"
+fi
+
+if [ -z "$2" ]; then
+    read -p "Enter your goal:" goal
+else
+    goal="$2"
+fi
+
+if [[ $stake -lt 1 ]]; then
+    echo "Sorry cannot start empty handed!"
+    exit 1
+fi
+
+win=0
+loss=0
+totalgambles=0
+while [[ ! "$stake" == 0 ]] || [[ ! "$stake" == "$goal" ]]; do
+    gamble=$(shuf -i 0-1 -n 1) # random of 0,1
+    totalgambles=$(( $totalgambles + 1 ))
+    if [[ "$gamble" == 0 ]]; then
+        [[ $stake -lt $goal ]] && stake=$(( $stake + 1 ))
+        win=$(( $win + 1 ))
+    else
+        [[ $stake -gt 0 ]] && stake=$(( $stake - 1 ))
+        loss=$(( $loss + 1 ))
+    fi
+    # stop conditions
+    [[ $stake == 0 ]] && break
+    [[ $stake == $goal ]] && break
+done
+echo "You won $win times."
+echo "You lost $loss times."
+echo "Total gambles: $totalgambles"
+winp=$(awk "BEGIN {print (($win/$totalgambles)*100)}")
+lossp=$(awk "BEGIN {print (100-$winp)}")
+echo "Win %age: $winp"
+echo "Loss %age: $lossp"
