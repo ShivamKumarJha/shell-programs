1d9203f05c8e6a857829052d6330c1aab3387792
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-17 16:40:12 +0530

    [ShivamKumarJha] Refactor crossgame.sh: fix condition mistakes
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- 4.crossgame.sh
+++ 4.crossgame.sh
@@ -19,59 +19,85 @@ while [[ 1 -gt 0 ]]; do
     # user i/p
     read -p "Enter choice: " ch
     index=$ch
-    if [[ ${sq[$index]} != "X" ]]; then
+    if [[ ${sq[$index]} != "X" ]] && [[ ${sq[$index]} != "O" ]]; then
         sq[$index]="X"
     else
         echo "Already has data. Repeating"
+        continue
     fi
     # computer choice
     comp=$(shuf -i 1-9 -n 1)
     index=$comp
-    if [[ ${sq[$index]} != "O" ]]; then
+    if [[ ${sq[$index]} != "X" ]] && [[ ${sq[$index]} != "O" ]]; then
         sq[$index]="O"
     else
         echo "Already has data. Repeating"
+        continue
     fi
     printboard
     # win conditions for computer
-    if [[ ${sq[1]} == "O" ]] && [[ ${sq[2]} == "O" ]] & [[ ${sq[3]} == "O" ]]; then
+    if [[ ${sq[1]} == "O" ]] && [[ ${sq[2]} == "O" ]] && [[ ${sq[3]} == "O" ]]; then
         echo "Compter wins!"
         break
     fi
-    if [[ ${sq[4]} == "O" ]] && [[ ${sq[5]} == "O" ]] & [[ ${sq[6]} == "O" ]]; then
+    if [[ ${sq[4]} == "O" ]] && [[ ${sq[5]} == "O" ]] && [[ ${sq[6]} == "O" ]]; then
         echo "Compter wins!"
         break
     fi
-    if [[ ${sq[7]} == "O" ]] && [[ ${sq[8]} == "O" ]] & [[ ${sq[9]} == "O" ]]; then
+    if [[ ${sq[7]} == "O" ]] && [[ ${sq[8]} == "O" ]] && [[ ${sq[9]} == "O" ]]; then
         echo "Compter wins!"
         break
     fi
-    if [[ ${sq[1]} == "O" ]] && [[ ${sq[5]} == "O" ]] & [[ ${sq[9]} == "O" ]]; then
+    if [[ ${sq[1]} == "O" ]] && [[ ${sq[4]} == "O" ]] && [[ ${sq[7]} == "O" ]]; then
+        echo "Compter wins!"
+        break
+    fi
+    if [[ ${sq[2]} == "O" ]] && [[ ${sq[5]} == "O" ]] && [[ ${sq[8]} == "O" ]]; then
+        echo "Compter wins!"
+        break
+    fi
+    if [[ ${sq[3]} == "O" ]] && [[ ${sq[6]} == "O" ]] && [[ ${sq[9]} == "O" ]]; then
+        echo "Compter wins!"
+        break
+    fi
+    if [[ ${sq[1]} == "O" ]] && [[ ${sq[5]} == "O" ]] && [[ ${sq[9]} == "O" ]]; then
         echo "Computer wins!"
         break
     fi
-    if [[ ${sq[3]} == "O" ]] && [[ ${sq[5]} == "O" ]] & [[ ${sq[7]} == "O" ]]; then
+    if [[ ${sq[3]} == "O" ]] && [[ ${sq[5]} == "O" ]] && [[ ${sq[7]} == "O" ]]; then
         echo "Computer wins!"
         break
     fi
     # win conditions for user
-    if [[ ${sq[1]} == "X" ]] && [[ ${sq[2]} == "X" ]] & [[ ${sq[3]} == "X" ]]; then
+    if [[ ${sq[1]} == "X" ]] && [[ ${sq[2]} == "X" ]] && [[ ${sq[3]} == "X" ]]; then
         echo "User wins!"
         break
     fi
-    if [[ ${sq[4]} == "X" ]] && [[ ${sq[5]} == "X" ]] & [[ ${sq[6]} == "X" ]]; then
+    if [[ ${sq[4]} == "X" ]] && [[ ${sq[5]} == "X" ]] && [[ ${sq[6]} == "X" ]]; then
         echo "User wins!"
         break
     fi
-    if [[ ${sq[7]} == "X" ]] && [[ ${sq[8]} == "X" ]] & [[ ${sq[9]} == "X" ]]; then
+    if [[ ${sq[7]} == "X" ]] && [[ ${sq[8]} == "X" ]] && [[ ${sq[9]} == "X" ]]; then
         echo "User wins!"
         break
     fi
-    if [[ ${sq[1]} == "X" ]] && [[ ${sq[5]} == "X" ]] & [[ ${sq[9]} == "X" ]]; then
+    if [[ ${sq[1]} == "X" ]] && [[ ${sq[4]} == "X" ]] && [[ ${sq[7]} == "X" ]]; then
         echo "User wins!"
         break
     fi
-    if [[ ${sq[3]} == "X" ]] && [[ ${sq[5]} == "X" ]] & [[ ${sq[7]} == "X" ]]; then
+    if [[ ${sq[2]} == "X" ]] && [[ ${sq[5]} == "X" ]] && [[ ${sq[8]} == "X" ]]; then
+        echo "User wins!"
+        break
+    fi
+    if [[ ${sq[3]} == "X" ]] && [[ ${sq[6]} == "X" ]] && [[ ${sq[9]} == "X" ]]; then
+        echo "User wins!"
+        break
+    fi
+    if [[ ${sq[1]} == "X" ]] && [[ ${sq[5]} == "X" ]] && [[ ${sq[9]} == "X" ]]; then
+        echo "User wins!"
+        break
+    fi
+    if [[ ${sq[3]} == "X" ]] && [[ ${sq[5]} == "X" ]] && [[ ${sq[7]} == "X" ]]; then
         echo "User wins!"
         break
     fi
