20be2060a5e017c4e5d28482d07ce82799d90479
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-18 15:28:10 +0530

    [ShivamKumarJha] Refactor coupon.sh: Use arrays instead of storing coupons to a text file.
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- 2.coupon.sh
+++ 2.coupon.sh
@@ -6,7 +6,7 @@ else
     num="$1"
 fi
 
-rm -rf coupons.txt
+array=()
 count=0
 distinct=0
 while [[ $distinct -lt $num ]]; do
@@ -14,20 +14,17 @@ while [[ $distinct -lt $num ]]; do
     value=$(awk "BEGIN {print ($randnum*($num-1))}")
     echo "Random Number Generated: $value"
     count=$(( $count + 1 ))
-    if cat coupons.txt |  grep "^$value"; then
+    if [[ " ${array[@]} " =~ " ${value} " ]]; then
         echo "Old value. Skipping."
     else
-        echo "New value. Writing..."
-        echo $value >> coupons.txt
+        echo "New value. Adding..."
+        array+=($value)
         distinct=$(( $distinct + 1 ))
     fi
 done
-
-# sort coupons list
-sort -n -o coupons.txt coupons.txt
 
 # display coupons generated
 echo "$count coupons generated."
 echo "$distinct distinct coupons generated."
 echo "Generated coupons:"
-cat coupons.txt
+echo ${array[@]}
