3ac5e7a3d4446198b05174addae7f3454764ab49
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-18 12:36:45 +0530

    [ShivamKumarJha] Refactor treewide: enable debug in shebang by default.
     * for without debug use `bash <script name>`
     * also update year variable in leapyear.sh
    
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- 3.leapyear.sh
+++ 3.leapyear.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash -x
 
 if [ -z "$1" ] ; then
-    read -p "Enter year: " yearvar
+    read -p "Enter year: " year
 else
-    yearvar="$1"
+    year="$1"
 fi
 
-if [[ $( echo ${yearvar} | wc -m ) == 5 ]]; then
+if [[ $( echo ${year} | wc -m ) == 5 ]]; then
     echo "Proceeding..."
-    if [[ $(( ${yearvar} % 400 )) == 0 ]]; then
-        echo "${yearvar} is a Leap Year"
-    elif [[ $(( ${yearvar} % 4 )) == 0 ]] && [[ $(( ${yearvar} % 100 )) != 0 ]]; then
-        echo "${yearvar} is a Leap Year"
+    if [[ $(( ${year} % 400 )) == 0 ]]; then
+        echo "${year} is a Leap Year"
+    elif [[ $(( ${year} % 4 )) == 0 ]] && [[ $(( ${year} % 100 )) != 0 ]]; then
+        echo "${year} is a Leap Year"
     else
-        echo "${yearvar} is NOT a Leap Year"
+        echo "${year} is NOT a Leap Year"
     fi
 else
     echo "Invalid year."
